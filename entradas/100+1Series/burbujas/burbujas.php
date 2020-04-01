806e59f7d9b3969dcef34abbf69afd91e755e52f
Pako Gonzalez <francisko.gonzalez@gmail.com>
2020-04-01 19:38:12 +0200

    Ejemplo de algoritmo de la burbuja simple

--- entradas/100+1Series/burbujas/burbujas.php
+++ entradas/100+1Series/burbujas/burbujas.php
@@ -1,9 +1,9 @@
 <?php
-
+$timeStart = new DateTime();
 $lista = [ 8, 5, 1, 6, 4, 3, 7, 2 ];
-print_r($lista);
+echo "Lista : ".implode(',', $lista)."\n";
 $n = count($lista);
-
+$paso = 0;
 for ( $i = 1; $i < $n-1; $i++ ){
 	for( $j = 0; $j < $n-$i; $j++  ){
 		if( $lista[ $j ] > $lista[ $j+1 ] ){
@@ -11,6 +11,11 @@ for ( $i = 1; $i < $n-1; $i++ ){
 			$lista[$j] = $lista[$j+1];
 			$lista[$j+1] = $temp;
 		}
+		$paso++;
+		echo "Paso $paso: ".implode(',', $lista)."\n";
 	}
 }
-print_r($lista);
+$timeEnd = new DateTime();
+echo "Orden  : ".implode(',', $lista)."\n";
+$intervalo = ($timeEnd->format('u') - $timeStart->format('u'))/1000000;
+echo $intervalo." segundos\n";
